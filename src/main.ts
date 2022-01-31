56819e8374931422d71b27927e086ca5e36f0b6d
iradukunda irenee <iradukundairenee9@gmail.com>
2022-01-31 10:12:24 +0200

    swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import {RolesGuard} from '../src/auth/guards/roles.guard'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Uzi App')
+  .setDescription('The Uzi backend Api')
+  .setVersion('1.0')
+  .addTag('user')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
